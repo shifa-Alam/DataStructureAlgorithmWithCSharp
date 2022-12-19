2322b8ad9061d8de80a14094544b2ee4358548b1
shifa-Alam <shifa7446@gmail.com>
2022-12-19 22:17:18 +0600

    timeComplexity example added

---
+++ dataStructureAlgorithm/TimeComplexity/Program.cs
@@ -0,0 +1,79 @@
+﻿
+//Example();
+//Example1();
+Example2();
+//Example3();
+
+void Example()
+{
+    int a = 10, b = 20, result = 0;
+
+    result = a + b;
+
+    Console.WriteLine(result);
+
+    // time complexity O(1)
+
+}
+
+
+
+
+void Example1()
+{
+    int n, result = 0;
+
+    n = Convert.ToInt32(Console.ReadLine());
+
+
+    result = n * (n + 1) / 2;
+
+    Console.WriteLine(result);
+
+    // time complexity O(1)
+
+}
+
+
+void Example2()
+{
+    int i, n, result = 0;
+
+    n = Convert.ToInt32(Console.ReadLine());
+
+    for (i = 1; i <= n; i++)
+    {
+        result = result + i;
+
+    }
+    Console.WriteLine(result);
+
+    // time complexity O(n)
+
+}
+
+
+// time complexity O(n^2)
+void Example3()
+{
+    int i, j, n, count = 0;
+
+    n = Convert.ToInt32(Console.ReadLine());
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            count = count + 1;
+        }
+    }
+    Console.WriteLine(count);
+
+    // time complexity O(n^2)
+
+}
+
+
+
+
+
