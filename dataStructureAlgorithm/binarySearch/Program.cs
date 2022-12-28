4a281109b2d195a8fd0cc5e0a9750be3a988566f
shifa-Alam <shifa7446@gmail.com>
2022-12-28 22:48:05 +0600

    linear and binary search added

---
+++ dataStructureAlgorithm/binarySearch/Program.cs
@@ -0,0 +1,26 @@
+﻿int binarySearch(int []a,int length, int searchNumber)
+{
+    int left=0;
+    int right= length - 1;
+    int mid;
+    while (left<=right)
+    {
+        mid = (left + right) / 2;
+        if (a[mid] == searchNumber)
+        {
+            return mid;
+        } 
+        if (a[mid] < searchNumber)
+        {
+            left=mid+1;
+        }
+        else
+        {
+            right=mid-1;
+        }
+    }
+    return -1;
+}
+int [] aa = { 1, 2, 3, 4,5, 6, 8 };
+var yy = binarySearch(aa, 7, 8);
+Console.WriteLine(yy);
---
+++ dataStructureAlgorithm/linearSearch/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int LinearSearch(int[] A,int x,int n)
+{
+    int i;
+    for ( i = 0; i < n; i++)
+    {
+        if (A[i] == x) return i;
+    }
+    i = -1;
+    return i;
+}
+int[] aa = { 1, 4, 5, 7, 9 };
+var x = LinearSearch(aa, 67, 5);
+Console.WriteLine(x);
