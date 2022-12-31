cf4d40a5d864dc04c876b25a560fb111dfca71ae
shifa-Alam <shifa7446@gmail.com>
2022-12-31 15:46:07 +0600

    BubbleSort Added

---
+++ dataStructureAlgorithm/BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿
+void BubbleSort(int[]a,int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n-i-1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j];    
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+
+            }
+        }
+    }
+    for(int k=0; k < n; k++)
+    {
+        Console.Write(a[k]+" ");
+    }
+}
+
+int[] x = { 90, 10, 32, 1, 2, 79 };
+BubbleSort(x, 6);
