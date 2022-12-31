d7b9761af6b6c32df6ab26fe94d313d7056e7e7c
shifa-Alam <shifa7446@gmail.com>
2022-12-31 15:23:50 +0600

    selection sort added

---
+++ dataStructureAlgorithm/SelectionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿void SelectionSort(int[]a, int n)
+{
+    int i, j, minIndex;
+
+    for (i = 0; i < n; i++)
+    {
+        minIndex = i;
+
+        for (j = i+1; j < n; j++)
+        {
+            if(a[j] < a[minIndex])
+            {
+                minIndex=j;
+            }
+            if (minIndex != i)
+            {
+                int temp=a[i];
+                a[i]=a[minIndex];   
+                a[minIndex]=temp;
+
+            }
+        }
+        Console.Write(a[i] + " ");
+    }
+    
+}
+int[] b = { 1, 10, 3, 32, 5 };
+SelectionSort(b, 5);
