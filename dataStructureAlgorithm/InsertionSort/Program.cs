5118deb4d310b07818779fb3396b99000f24a032
shifa-Alam <shifa7446@gmail.com>
2023-01-01 16:01:06 +0600

    InsertionSort Added

---
+++ dataStructureAlgorithm/InsertionSort/Program.cs
@@ -0,0 +1,22 @@
+﻿void InsertionSort(int[] a)
+{
+    int i, j, item;
+    for(i = 1; i < a.Length; i++)
+    {
+        item = a[i];
+        j = i - 1;
+        while(j >= 0 && a[j] > item)
+        {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = item;
+    }
+
+    for(i = 0; i < a.Length; i++)
+    {
+        Console.Write(a[i] +" ");
+    }
+}
+int[] x = { 5,4,2,3,1 };
+InsertionSort(x);
